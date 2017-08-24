677054844e92f51001a5080a33aab42fc120622e
dos09 <fake@fake.com>
2017-08-25 00:09:48 +0300

    Update gitignore, temporary skip, limit example

--- test.py
+++ test.py
@@ -1,25 +1,37 @@
-class A:
-    def __init__(self):
-        self.attr = {'edno':111, 'dve':222, 'tri':333}
-    
-    @property
-    def edno(self):
-        return self.attr.get('edno')
-    
-    @property
-    def x(self):
-        return self._x
-    
-    @x.setter
-    def x(self, x):
-        if x < 0:
-            x = 0
-        elif x > 1000:
-            x = 1000
-        self._x = x
-    
-a = A()
-print(a.edno)
+import pymongo
 
-str = "I am {0:s} banana".format("yellow")
-print(str)
+
+def print_in_batches(db):
+    limit = 2
+    offset = 0
+    while True:
+        data = list(db.nums.find({}).skip(offset).limit(limit))
+        if not data:
+            break
+        end = offset + limit
+        print('Fetched data from %s to %s' % (offset, end))
+        print(data)
+        offset = end
+
+
+def modify_in_batches(db):
+    limit = 2
+    while True:
+        # no skipping is needed because we change the found documents
+        # so they won't match the search criteria
+        docs = list(db.nums.find({'value': {'$gte': 1}}).limit(limit))
+        if not docs:
+            break
+
+        print(docs)
+
+        for doc in docs:
+            doc['value'] = 0
+            db.nums.replace_one({'_id': doc['_id']}, doc)
+
+db = pymongo.MongoClient('mongodb://localhost:27017')['mytest']
+count = db.nums.find({}).count()
+print(count, 'records found in nums collection')
+
+# print_in_batches(db)
+modify_in_batches(db)
