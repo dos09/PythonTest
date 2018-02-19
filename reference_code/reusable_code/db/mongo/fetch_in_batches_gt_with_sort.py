5e3894bc80733e5029d818d3ccc6648e2e44c3d0
Zhulien Yankov <zhulien.yankov@sirma.bg>
2018-02-19 16:11:15 +0200

    Update fetch_in_batches $gt mongo

--- reference_code/reusable_code/db/mongo/fetch_in_batches_gt_with_sort.py
+++ reference_code/reusable_code/db/mongo/fetch_in_batches_gt_with_sort.py
@@ -16,7 +16,6 @@ LOG.addHandler(ch)
 LOG.setLevel('DEBUG')
 
 
-
 def get_mongo_db_con(url, db_name):
     return getattr(pymongo.MongoClient(url), db_name)
 
@@ -34,31 +33,34 @@ def retry_run(_callable, *args, **kwargs):
     raise TimeoutError()
 
 
-
-
-def batch_fetch_using_sort(db, collection_name,
-                           find_query, fetch_fields, sort_by_field='_id',
+def batch_fetch_using_sort(db, collection_name, find_query=None,
+                           fetch_fields=None, sort_by_field='_id',
                            batch_limit=10000, matching_docs_count=None):
     """ Fetch data in batches.
-     
+
     Instead of skip/limit batch fetching which is slow for large volumes
     of data, use the following approach:
     1. Filter by <find_query>, sort in ascending order by 
-    <sort_by_field> and take the first document from the result set 
-    2. Now add to <find_query> the condition <sort_by_field> to be > X, where X is
-    the value taken from the <sort_by_field> of the fetched document. 
-    3. Limit the count of the fetched documents and while data is fetched 
-    replace the used value for X with the last fetched document's <sort_by_field>
+    <sort_by_field> and take one batch of documents.
+    2. Now add to <find_query> the condition <sort_by_field> to be > X, 
+    where X is the value taken from the <sort_by_field> of the most 
+    recently fetched document (last document of each batch).
+    3. Repeat step 2 while data is available 
 
     !!! ATTENTION !!!
-        <sort_by_field> must be unique ($gt is used), and is required
-        to exist in all documents
-    
+        <sort_by_field> must be unique ($gt is used), and MUST
+        exist in all documents
+
     Yield a batch of documents
     """
     def query(limit_size):
         return list(collection.find(find_query, fetch_fields)
                     .sort(sort_by).limit(limit_size))
+
+    if find_query is None:
+        find_query = {}
+    if fetch_fields is None:
+        fetch_fields = {}
 
     collection = db[collection_name]
     find_query = dict(find_query)
@@ -72,14 +74,6 @@ def batch_fetch_using_sort(db, collection_name,
              collection_name, matching_docs_count)
     if not matching_docs_count:
         return
-    docs = retry_run(query, 1)
-    if not docs:
-        LOG.warning('There were documents matching the '
-                    'filter but now nothing is found')
-        return
-
-    yield docs
-    find_query[sort_by_field] = {'$gt': docs[0][sort_by_field]}
     processed_docs_count = 1
     progress_text = (r'Processed {0:>%d}/%d documents from %s [{1:6.2f}%%]' %
                      (len(str(matching_docs_count)), matching_docs_count,
@@ -89,7 +83,8 @@ def batch_fetch_using_sort(db, collection_name,
         if not docs:
             break
         yield docs
-        find_query[sort_by_field]['$gt'] = docs[-1][sort_by_field]
+
+        find_query[sort_by_field] = {'$gt': docs[-1][sort_by_field]}
         processed_docs_count += len(docs)
         percent = (processed_docs_count / matching_docs_count) * 100
         LOG.info(progress_text.format(processed_docs_count, percent))
@@ -97,10 +92,10 @@ def batch_fetch_using_sort(db, collection_name,
 
 if __name__ == '__main__':
     mongo_url = 'mongodb://localhost:27017'
-    mongo_db_name = 'rimm' 
+    mongo_db_name = 'rimm'
     db = get_mongo_db_con(mongo_url, mongo_db_name)
     for docs in batch_fetch_using_sort(
-            db=db, collection_name='vulnerabilitiesGrid', 
-            _find_query = {'related.threatModelSims': '3uzC6dv9AKE6M5m4L'}, 
-            _fetch_fields = {'info.name': 1}):
+            db=db, collection_name='vulnerabilitiesGrid',
+            _find_query={'related.threatModelSims': '3uzC6dv9AKE6M5m4L'},
+            _fetch_fields={'info.name': 1}):
         print(docs)
